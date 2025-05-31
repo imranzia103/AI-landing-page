f45a25bdb212e2db1647a9acf633ac3e243cb863
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-06-01 00:28:43 +0500

    slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-
+import { useState } from "react";
+import PhotoScroll from "./PhotoScroll"
+import SearchInput from "./SearchInput";
 
 import { Layout } from "./components/Layout"
-import { Hero } from "./components/sections/Hero"
+//import { Hero } from "./components/sections/Hero"
 const App = () => {
-  return <Layout title="EdgeAI">
+
+ const [data, setData] = useState<{imageData:string[]}>({imageData:[]})
+
+  const setImageData = (data:string[]) => {
+    setData({imageData:data})
+  }
 
 
-    <Hero />
-  </Layout>
+
+
+
+
+  return (
+    <Layout title="EdgeAI">
+    
+    <div className="App w-screen h-screen flex flex-col items-center overflow-hidden">
+      <h1 className="text-center py-4 text-2xl font-bold">React Photo Scroll</h1>
+      <SearchInput setImageData={setImageData}></SearchInput>
+      <PhotoScroll imageData={data.imageData}></PhotoScroll>
+    </div>
+  //</Layout>
+  )
+  
+  
+
+
 }
 
 export default App
